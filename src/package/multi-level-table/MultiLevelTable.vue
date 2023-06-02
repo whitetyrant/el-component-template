7f698fba9bcd3324c1902abfe8c4fa5c38f4b12d
whitetyrant114 <1059079377@qq.com>
2023-06-02 16:04:04 +0800

    1.改loading

--- src/package/multi-level-table/MultiLevelTable.vue
+++ src/package/multi-level-table/MultiLevelTable.vue
@@ -31,7 +31,7 @@ export default {
     },
     props: {
         isLoading: {
-            type: 'boolean',
+            type: Boolean,
             default: false,
         },
         tableData: {
